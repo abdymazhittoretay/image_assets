a8ac72f84235386d859462999812faf19e2927ba
Toretay <abdymvzhit@gmail.com>
2025-02-24 14:03:30 +0500

    added image assets with network images in listview

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
 
@@ -23,6 +20,24 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        surfaceTintColor: Colors.white,
+        title: Text("Images"),
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Image.network(
+              "https://images.unsplash.com/photo-1727467082310-095b43d11449?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            ),
+            Image.asset("images/image-1.jpg"),
+            Image.asset("images/image-2.jpg"),
+            Image.asset("images/image-3.jpg"),
+          ],
+        ),
+      ),
     );
   }
 }
